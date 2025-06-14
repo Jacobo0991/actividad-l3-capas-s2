e62adff6ac559dea09523254afdb65c502c5353c
Jacobo0991 <00005822@uca.edu.sv>
2025-06-14 09:19:14 -0600

    Agrega dependencias

---
+++ labo03/src/main/java/com/labo03/labo03/Labo03Application.java
@@ -0,0 +1,13 @@
+package com.labo03.labo03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labo03Application.class, args);
+    }
+
+}
